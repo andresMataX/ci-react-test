d15307e273d17a40dd48f73f8b2dc1aacbc1bb8c
Jesús Mata <andresmata.c21@gmail.com>
2023-02-25 23:16:33 -0600

    Testing with screen

--- test/FirstApp.test.tsx
+++ test/FirstApp.test.tsx
@@ -1,39 +1,30 @@
 /** @jest-environment jsdom */
 import React from 'react'
 import { describe, test, expect } from '@jest/globals'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { FirstApp } from '../src/FirstApp'
 
 describe('Testing in <FirstApp.test />', () => {
-  // test('debe de hacer match con el snapshot', () => {
-  //   const title = 'Hola, pepe'
+  const title = 'Hola, pepe'
+  const subtitle = 'Soy un subtítulo'
 
-  //   const { container } = render(<FirstApp title={title} subtitle={12} />)
-
-  //   expect(container).toMatchSnapshot()
-  // })
-
-  test('should de mostrar el título en un h1', () => {
-    const title = 'Hola, pepe'
-    const { container, getByText, getByTestId } = render(
-      <FirstApp title={title} subtitle={'Soy un sub'} />
-    )
-    expect(getByText(title)).toBeTruthy()
-
-    // const h1 = container.querySelector('h1')
-    // expect(h1?.innerHTML).toContain(title)
-
-    expect(getByTestId('test-title').innerHTML).toContain(title)
+  test('should hacer match con el snapshot', () => {
+    const { container } = render(<FirstApp title={title} />)
+    expect(container).toMatchSnapshot()
   })
 
-  test('debe de mostrar el subtítulo enviado por props', () => {
-    const title = 'Hola, pepe'
-    const subtitle = 'Soy un sub'
-    const { getByText, getAllByText } = render(
-      <FirstApp title={title} subtitle={subtitle} />
-    )
+  test('should mostrar el mensaje "Hola, pepe"', () => {
+    render(<FirstApp title={title} />)
+    expect(screen.getByText(title)).toBeTruthy()
+  })
 
-    // expect(getByText(subtitle)).toBeTruthy()
-    expect(getAllByText(subtitle).length).toBe(2)
+  test('should mostrar el título en un h1', () => {
+    render(<FirstApp title={title} />)
+    expect(screen.getByRole('heading', { level: 1 }).innerHTML).toContain(title)
+  })
+
+  test('should mostrar el subtítulo pasado por props', () => {
+    render(<FirstApp title={title} subtitle={subtitle} />)
+    expect(screen.getAllByText(subtitle).length).toBe(2)
   })
 })
