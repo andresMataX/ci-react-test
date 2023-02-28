ad933e8e6dfeee7b74d151a4a59d90d76129342a
Jesús Mata <andresmata.c21@gmail.com>
2023-02-27 22:16:02 -0600

    Testing clicks

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,41 +1,36 @@
 import { useState } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value)
 
-    const [counter, setCounter] = useState(value);
+  const handleAdd = () => {
+    // setCounter(counter + 1)
+    // Callback que recibe el valor actual, lo usamos para aumentar el estado en uno
+    setCounter((c) => c + 1)
+  }
 
-    const handleAdd = () => {
-        // setCounter(counter + 1)
-        // Callback que recibe el valor actual, lo usamos para aumentar el estado en uno
-        setCounter((c) => c + 1);
-    }
+  const handleReset = () => {
+    setCounter(value)
+  }
 
-    const handleReset = () => {
-        setCounter(value);
-    }
+  const handleSubtract = () => {
+    setCounter(counter - 1)
+  }
 
-    const handleSubtract = () => {
-        setCounter(counter - 1);
-    }
-
-    return (
-        <>
-            <h1>CounterApp</h1>
-            <h2>{counter}</h2>
-            <button onClick={handleAdd}>
-                +1
-            </button>
-            <button onClick={handleReset}>
-                Reset
-            </button>
-            <button onClick={handleSubtract}>
-                -1
-            </button>
-        </>
-    )
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{counter}</h2>
+      <button onClick={handleAdd}>+1</button>
+      <button aria-label="btn-reset" onClick={handleReset}>
+        Reset
+      </button>
+      <button onClick={handleSubtract}>-1</button>
+    </>
+  )
 }
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
+  value: PropTypes.number.isRequired,
 }
--- test/CounterApp.test.tsx
+++ test/CounterApp.test.tsx
@@ -1,7 +1,7 @@
 /** @jest-environment jsdom */
 import React from 'react'
 import { describe, test, expect } from '@jest/globals'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { CounterApp } from '../src/CounterApp'
 
 describe('Testing in <CounterApp.test />', () => {
@@ -15,5 +15,29 @@ describe('Testing in <CounterApp.test />', () => {
   test('debe de mostrar el valor inicial de 100', () => {
     render(<CounterApp value={value} />)
     expect(screen.findByText(value)).toBeTruthy()
+  })
+
+  test('debe incrementar con el botón +1', () => {
+    render(<CounterApp value={value} />)
+    fireEvent.click(screen.getByText('+1'))
+    expect(screen.getByText('101')).toBeTruthy()
+  })
+
+  test('debe incrementar con el botón -1', () => {
+    render(<CounterApp value={value} />)
+    fireEvent.click(screen.getByText('-1'))
+    expect(screen.getByText('99')).toBeTruthy()
+  })
+
+  test('debe funcionar el botón de reset', () => {
+    render(<CounterApp value={value} />)
+    fireEvent.click(screen.getByText('+1'))
+    fireEvent.click(screen.getByText('+1'))
+    fireEvent.click(screen.getByText('+1'))
+    // fireEvent.click(screen.getByText('Reset'))
+
+    fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }))
+
+    expect(screen.getByText(value)).toBeTruthy()
   })
 })
